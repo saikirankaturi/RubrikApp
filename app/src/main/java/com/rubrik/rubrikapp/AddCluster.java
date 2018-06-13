9c249b0b0a2e0bef0492553a134c59e791a1c950
Sai Kiran Katuri <kirankingiit@gmail.com>
2018-06-13 23:39:01 +0530

    Add cluster related things

--- app/src/main/java/com/rubrik/rubrikapp/AddCluster.java
+++ app/src/main/java/com/rubrik/rubrikapp/AddCluster.java
@@ -1,15 +1,19 @@
 package com.rubrik.rubrikapp;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import com.rubrik.rubrikapp.RestApi.SwaggerObject.BrikCount;
 import com.rubrik.rubrikapp.RestApi.JsonObjectRetriever;
 import com.rubrik.rubrikapp.RestApi.JsonObjectVolleyInterface;
+import com.rubrik.rubrikapp.Utils.GenericUtils;
 
 public class AddCluster extends AppCompatActivity {
 
@@ -33,10 +37,42 @@ public class AddCluster extends AppCompatActivity {
         );
     }
 
-    public void sendMessage(View view)
-    {
-        Intent intent = new Intent(AddCluster.this, mainlogin.class);
-        startActivity(intent);
+    public void sendMessage(View view) {
+        final EditText editText = (EditText)findViewById(R.id.ipaddress);
+        final String inputIp = editText.getText().toString(); //gets you the contents of edit text
+        if (!GenericUtils.isValidIpAddress(inputIp)) {
+            AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
+            dlgAlert.setMessage("Please enter a valid IP address");
+            dlgAlert.setTitle("Invalid IP");
+            dlgAlert.setCancelable(false);
+            dlgAlert.setPositiveButton("Ok",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            findViewById(R.id.ipaddress).refreshDrawableState();
+                            editText.setText("");
+                        }
+                    });
+            dlgAlert.create().show();
+        } else if (!GenericUtils.isIpAccessibleOver443(inputIp)) {
+            AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
+            dlgAlert.setMessage("Please enter a valid node's IP address");
+            dlgAlert.setTitle("Can't establish connection");
+            dlgAlert.setCancelable(false);
+            dlgAlert.setPositiveButton("Ok",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            findViewById(R.id.ipaddress).refreshDrawableState();
+                            editText.setText("");
+                        }
+                    });
+            dlgAlert.create().show();
+        } else {
+            Log.d("Woah!!!", inputIp);
+            Intent intent = new Intent(AddCluster.this, mainlogin.class);
+            startActivity(intent);
+        }
     }
 
 }
