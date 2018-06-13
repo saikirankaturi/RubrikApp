e1ebb88125db607364ffb5f2a13f075352dd90f9
mitesh-agrawal <contactmiteshag@gmail.com>
2018-06-13 23:36:37 +0530

    push notification changes

--- app/src/main/java/com/rubrik/rubrikapp/AddCluster.java
+++ app/src/main/java/com/rubrik/rubrikapp/AddCluster.java
@@ -1,8 +1,16 @@
 package com.rubrik.rubrikapp;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.android.volley.RequestQueue;
@@ -10,6 +18,8 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 import org.json.JSONObject;
 
@@ -23,6 +33,9 @@ public class AddCluster extends AppCompatActivity {
 
     public void sendMessage(View view)
     {
+        String token = FirebaseInstanceId.getInstance().getToken();
+        Log.d("AddCluster token", "token = " + token);
+
         Intent intent = new Intent(AddCluster.this, mainlogin.class);
         startActivity(intent);
     }
---
+++ app/src/main/java/com/rubrik/rubrikapp/PushNotificationListenService.java
@@ -0,0 +1,81 @@
+package com.rubrik.rubrikapp;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class PushNotificationListenService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.d("PushNotificationListen", "got message from: " + remoteMessage.getFrom());
+        if(remoteMessage.getData().size() > 0){
+            Log.d("PushNotificationListen", "message data: " + remoteMessage.getData());
+        }
+        if(remoteMessage.getNotification() != null){
+            Log.d("PushNotificationListen", "message body: " + remoteMessage.getNotification().getBody());
+            sendNotification(remoteMessage.getNotification().getBody());
+        }
+    }
+
+    private void sendNotification(String body) {
+//        Intent intent = new Intent(this, mainlogin.class);
+//        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+//
+//        Uri notificationSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+//
+//        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "af")
+//                .setSmallIcon(R.mipmap.ic_launcher)
+//                .setContentTitle("Notification checker")
+//                .setContentText(body)
+//                .setAutoCancel(true)
+//                .setSound(notificationSound)
+//                .setContentIntent(pendingIntent);
+//
+//        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+//        notificationManager.notify(0, notificationBuilder.build());
+
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "my_channel_id_01";
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "My Notifications", NotificationManager.IMPORTANCE_MAX);
+
+            // Configure the notification channel.
+            notificationChannel.setDescription("Channel description");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.enableVibration(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+
+        notificationBuilder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setTicker("Hearty365")
+                //     .setPriority(Notification.PRIORITY_MAX)
+                .setContentTitle("Default notification")
+                .setContentText(body)
+                .setContentInfo("Info");
+
+        notificationManager.notify(/*notification id*/1, notificationBuilder.build());
+    }
+}
