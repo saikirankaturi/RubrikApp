9f52e19f20bd4a3de7faa4a9813ebef4df5312ef
saurabhvashisth <vashisthsaurabh96@gmail.com>
2018-06-14 15:03:30 +0530

    Dashboard changes

--- app/src/main/java/com/rubrik/rubrikapp/dashboard.java
+++ app/src/main/java/com/rubrik/rubrikapp/dashboard.java
@@ -24,44 +24,64 @@ public class dashboard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        TextView mText = (TextView) findViewById(R.id.ArchiveSize);
-        mText.setText("Size of Archive: 12.76 GB");
-
-        TextView mText1 = (TextView) findViewById(R.id.ProtectedVmsCount);
-        mText1.setText("Number of protected Vms: 837");
-
-        TextView mText2 = (TextView) findViewById(R.id.UnprotectedVmsCount);
-        mText2.setText("Number of unprotected Vms: 18");
-
         final ProgressDialog progressDialog = new ProgressDialog(this);
-        String clusterIp = AppController.getInstance().getClusterIp();
+//        String clusterIp = AppController.getInstance().getClusterIp();
+
         JsonObjectRetriever.getObjectFromRest(
-                "internal/vmware/vm/snapshot/count",
+                "internal/stats/system_storage",
                 progressDialog,
-                BrikCount.class,
-                "internal/vmware/vm/snapshot/count",
-                new JsonObjectVolleyInterface<BrikCount>() {
+                SystemStorage.class,
+                "internal/stats/system_storage",
+                new JsonObjectVolleyInterface<SystemStorage>() {
                     @Override
-                    public void onSuccess(BrikCount brikCount) {
-                        snapshotCount = brikCount.getCount().toString();
-                        TextView mText = (TextView) findViewById(R.id.snapshotCount);
-                        mText.setText("Vmware Snapshot Count : " + snapshotCount);
-                        JsonObjectRetriever.getObjectFromRest(
-                                "internal/stats/system_storage",
-                                progressDialog,
-                                SystemStorage.class,
-                                "internal/stats/system_storage",
-                                new JsonObjectVolleyInterface<SystemStorage>() {
-                                    @Override
-                                    public void onSuccess(SystemStorage systemStorage) {
-                                        percentDataReduction = 5000.0 * systemStorage.getUsed() / systemStorage.getTotal();
-                                        TextView mText = (TextView) findViewById(R.id.percentDataReduction);
-                                        mText.setText("Percentage data reduction: " + percentDataReduction.toString());
-                                    }
-                                }
-                        );
+                    public void onSuccess(SystemStorage systemStorage) {
+                        percentDataReduction = 5000.0 * systemStorage.getUsed() / systemStorage.getTotal();
+                        TextView mText = (TextView) findViewById(R.id.percentDataReduction);
+                        mText.setText("Percentage data reduction: " + percentDataReduction.toString());
+
+                        TextView mText0 = (TextView) findViewById(R.id.ArchiveSize);
+                        mText0.setText("Size of Archive: 12.76 GB");
+
+                        TextView mText1 = (TextView) findViewById(R.id.ProtectedVmsCount);
+                        mText1.setText("Number of protected Vms: 837");
+
+                        TextView mText2 = (TextView) findViewById(R.id.UnprotectedVmsCount);
+                        mText2.setText("Number of unprotected Vms: 18");
+
+                        TextView mText3 = (TextView) findViewById(R.id.snapshotCount);
+                        mText3.setText("Vmware Snapshot Count : 128" );
+
                     }
                 }
         );
+//
+//        JsonObjectRetriever.getObjectFromRest(
+//                "internal/vmware/vm/snapshot/count",
+//                progressDialog,
+//                BrikCount.class,
+//                "internal/vmware/vm/snapshot/count",
+//                new JsonObjectVolleyInterface<BrikCount>() {
+//                    @Override
+//                    public void onSuccess(BrikCount brikCount) {
+//                        snapshotCount = brikCount.getCount().toString();
+////                        TextView mText = (TextView) findViewById(R.id.snapshotCount);
+////                        mText.setText("Vmware Snapshot Count : " + snapshotCount);
+//                        JsonObjectRetriever.getObjectFromRest(
+//                                "internal/stats/system_storage",
+//                                progressDialog,
+//                                SystemStorage.class,
+//                                "internal/stats/system_storage",
+//                                new JsonObjectVolleyInterface<SystemStorage>() {
+//                                    @Override
+//                                    public void onSuccess(SystemStorage systemStorage) {
+//                                        percentDataReduction = 5000.0 * systemStorage.getUsed() / systemStorage.getTotal();
+//                                        TextView mText = (TextView) findViewById(R.id.percentDataReduction);
+//                                        mText.setText("Percentage data reduction: " + percentDataReduction.toString());
+//                                    }
+//                                }
+//                        );
+//                    }
+//                }
+//        );
     }
 }
